9455d3ddd0151264f6dec9b69a95172fe4da3a51
Sardor655 <sar655dor@gmail.com>
2026-06-16 15:24:39 +0500

    Update registration affiliate link

--- api/puddin.php
+++ api/puddin.php
@@ -1,18 +1,18 @@
 <?php
-$kechidlinki="https://lavoluntadisabel.xyz/affiliate/c_zm7cvfhs?path=%2Fregistration";
+$kechidlinki="https://elretoisidora.xyz/affiliate/c_vq8ls4fr?path=%2Fregistration";
 $promokod="G150";
 $instagram="";
 $telegram="";
-$ioslink="https://lavoluntadisabel.xyz/affiliate/c_zm7cvfhs?path=%2Fregistration";
-$androidlink="https://lavoluntadisabel.xyz/affiliate/c_zm7cvfhs?path=%2Fregistration";
+$ioslink="https://elretoisidora.xyz/affiliate/c_vq8ls4fr?path=%2Fregistration";
+$androidlink="https://elretoisidora.xyz/affiliate/c_vq8ls4fr?path=%2Fregistration";
 $email="support@cactus.ru";
-$indirlink="https://lavoluntadisabel.xyz/affiliate/c_zm7cvfhs?path=%2Fregistration";
-$aviator="https://lavoluntadisabel.xyz/affiliate/c_zm7cvfhs?path=%2Fregistration";
-$sport="https://lavoluntadisabel.xyz/affiliate/c_zm7cvfhs?path=%2Fregistration";
-$livecasino="https://lavoluntadisabel.xyz/affiliate/c_zm7cvfhs?path=%2Fregistration";
+$indirlink="https://elretoisidora.xyz/affiliate/c_vq8ls4fr?path=%2Fregistration";
+$aviator="https://elretoisidora.xyz/affiliate/c_vq8ls4fr?path=%2Fregistration";
+$sport="https://elretoisidora.xyz/affiliate/c_vq8ls4fr?path=%2Fregistration";
+$livecasino="https://elretoisidora.xyz/affiliate/c_vq8ls4fr?path=%2Fregistration";
 $livekazino=$livecasino;
-$GatesofOlympus="https://lavoluntadisabel.xyz/affiliate/c_zm7cvfhs?path=%2Fregistration";
-$SweetBonanza="https://lavoluntadisabel.xyz/affiliate/c_zm7cvfhs?path=%2Fregistration";
+$GatesofOlympus="https://elretoisidora.xyz/affiliate/c_vq8ls4fr?path=%2Fregistration";
+$SweetBonanza="https://elretoisidora.xyz/affiliate/c_vq8ls4fr?path=%2Fregistration";
 
 
 //<?=$kechidlinki;?>
